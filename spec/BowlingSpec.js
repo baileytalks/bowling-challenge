bf8d67f77425d87edf27f99509afa96238c0dc50
Bailey Kursar <bkursar@gmail.com>
2017-05-21 20:39:23 +0100

    Frame 2 now exists and adds to the game score

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -6,4 +6,9 @@ describe('Game', function() {
     game.frame1(1,1);
     expect(game.score).toEqual(2);
   });
+
+  it('when frame2 equals 2, the game score is now 4', function() {
+    game.frame2(1,1);
+    expect(game.score).toEqual(4);
+  });
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -6,3 +6,8 @@ Game.prototype.frame1 = function (bowl1, bowl2) {
   frame1 = bowl1 + bowl2;
   this.score += frame1;
 };
+
+Game.prototype.frame2 = function (bowl3, bowl4) {
+  frame2 = bowl3 + bowl4;
+  this.score += frame2;
+};
