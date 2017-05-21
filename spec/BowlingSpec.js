59fddbe2904d6a1483c09e0d3b3c89bc0f255564
Bailey Kursar <bkursar@gmail.com>
2017-05-21 21:44:14 +0100

    A game can now only have 10 frames

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,14 +1,33 @@
-describe('Game', function() {
+describe('Bowling', function() {
 
   var game = new Game();
+  var newGame = new Game();
+
+  it('no bowls can take down more than 10 pins', function() {
+    expect(game.frame(11,11)).toEqual('error');
+  });
 
   it('when bowl1 and bowl2 both equal 1, frame1 equals 2', function() {
-    game.frame1(1,1);
+    game.frame(1,1);
     expect(game.score).toEqual(2);
   });
 
   it('when frame2 equals 2, the game score is now 4', function() {
-    game.frame2(1,1);
+    game.frame(1,1);
     expect(game.score).toEqual(4);
+  });
+
+  it('when all frames equal 2, the game score is 20', function() {
+    for (x = 0; x < 8; x++) {
+      game.frame(1,1);
+    };
+    expect(game.score).toEqual(20);
+  });
+
+  it('a game can only have 10 frames', function() {
+    for (x = 0; x < 10; x++) {
+      newGame.frame(1,1);
+    };
+    expect(newGame.frame(1,1)).toEqual('no more frames, end of game');
   });
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,13 +1,15 @@
 function Game() {
-  this.score = 0;
+  this.score  = 0;
+  this.frames = [];
 }
 
-Game.prototype.frame1 = function (bowl1, bowl2) {
-  frame1 = bowl1 + bowl2;
-  this.score += frame1;
-};
+Game.prototype.frame = function(a, b) {
+  if (a > 10) { return 'error' };
+  if (b > 10) { return 'error' };
 
-Game.prototype.frame2 = function (bowl3, bowl4) {
-  frame2 = bowl3 + bowl4;
-  this.score += frame2;
+  frameScore = a + b;
+  this.frames.push(frameScore);
+  this.score += frameScore;
+
+  if (this.frames.length === 11) { return 'no more frames, end of game' };
 };
